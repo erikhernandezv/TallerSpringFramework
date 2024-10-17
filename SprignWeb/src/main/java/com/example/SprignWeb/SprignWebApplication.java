08503d04d53506926143b8f372844693e40fc0a6
Erik Dario Hernandez Vasquez <ehvasque@bancolombia.com.co>
2024-10-17 18:55:17 -0500

    Archivos ejercicio Spring-Security

---
+++ SprignWeb/src/main/java/com/example/SprignWeb/SprignWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.SprignWeb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprignWebApplication {
+
+	public static void main(String[] args) throws Throwable {
+		SpringApplication.run(SprignWebApplication.class, args);
+	}
+
+}
---
+++ SprignWeb/src/test/java/com/example/SprignWeb/SprignWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.SprignWeb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SprignWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
